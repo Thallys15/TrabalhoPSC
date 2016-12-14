f3ab0b0afebcaa3aa1663f49ad0bae40b7ea5575
Thallys15 <thallys.lisboa15@gmail.com>
2016-12-14 01:16:24 -0200

    adicionando metodo closeconnection com tratamento de erro

--- Projeto/MasterClub_Persistencia/src/connection/ConnectionFactory.java
+++ Projeto/MasterClub_Persistencia/src/connection/ConnectionFactory.java
@@ -6,7 +6,10 @@
 package connection;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
 
 /**
  *
@@ -29,5 +32,35 @@ public class ConnectionFactory {
         }
 
     } 
-
+    public static void closeConnection(Connection con){
+      if (con != null){
+          try {
+              con.close();
+          } catch (SQLException ex) {
+              System.err.println("Erro: "+ex);
+          }
+      }
+    }
+    
+    public static void closeConnection(Connection con, PreparedStatement stmt){
+        if (stmt != null){
+             try {
+                    stmt.close();
+            } catch (SQLException ex) {
+              System.err.println("Erro: "+ex);
+        }
+            closeConnection(con);
+      }
+    }
+    
+    public static void closeConnection(Connection con, PreparedStatement stmt, ResultSet rs){
+        if (rs != null){
+             try {
+                    rs.close();
+            } catch (SQLException ex) {
+              System.err.println("Erro: "+ex);
+        }
+            closeConnection(con,stmt);
+      }
+    }
 }
