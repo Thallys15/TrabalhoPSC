787e606364fa1858d1fd8855777e7fd75c2630d5
Thallys15 <thallys.lisboa15@gmail.com>
2016-12-15 19:36:46 -0200

    Inserindo DAO AlugaDependencida na camada persistencia

--- Projeto/MasterClub_Persistencia/src/masterclub_persistencia/AlugaDependenciaDAO.java
+++ Projeto/MasterClub_Persistencia/src/masterclub_persistencia/AlugaDependenciaDAO.java
@@ -6,16 +6,55 @@
 package masterclub_persistencia;
 
 import connection.ConnectionFactory;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import masterclub_aplicacao.AlugaDependencia;
 
 /**
  *
  * @author tls15
  */
-public class AlugaDependenciaDAO {
-
+public class AlugaDependenciaDAO extends AlugaDependencia {
+            ConnectionFactory conn;
     public AlugaDependenciaDAO() {
             ConnectionFactory.getConnection();
     }
     
-    public
+    public AlugaDependenciaDAO Abrir(String cpf) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public boolean Salvar(AlugaDependenciaDAO obj) {
+        try {
+            if(obj.getClube() == 0){
+                // Objeto não está no BD, inserir
+
+                // Cria a consulta sql
+                PreparedStatement sql = conn.preparedStatement("INSERT INTO AlugaDependenciaDAO(clube,dependencia,tempo) "
+                        + "VALUES(?,?,?)");
+                
+                
+                // Passa os parâmetros para a consulta SQL
+                sql.setInt(1, obj.getClube());
+                sql.setInt(2, obj.getDependencia());
+                sql.setDate(3, new java.sql.Date(obj.getTempoAluguel().getTime()));
+                
+                
+                // Executa a consulta SQL
+                sql.executeUpdate();
+                
+                
+                
+            } else {
+                // Objeto já está no BD, atualizar
+            }
+            
+            return true;
+        
+        } catch(SQLException e){
+            System.out.print(e);
+            
+        }
+        return false;
+    }
 }
