9fa80c680a77323dc317e3578bd4d429a05f454d
igor0503 <igorpalmeida96@gmail.com.br>
2017-02-21 15:30:51 -0300

    correcao de erros do banco de dados

--- Banco de Dados/MasterClub.sql
+++ Banco de Dados/MasterClub.sql
@@ -25,20 +25,6 @@ use MasterClub;
         renovacao int,
         salario float
 	);
-	create table atleta(
-		id int primary key auto_increment,
-		posicao varchar(10),
-        lado varchar(10),
-        categoria varchar(10),
-        naturalidade varchar(50),
-        idade int,
-        altura float,
-        peso float,
-        cpf char(11),
-        rg char(10),
-        clube int,
-        foreign key (clube) references clube(cnpj)
-    );
     create table funcionario(
 		id int primary key auto_increment,
 		cpf char(11),
@@ -51,34 +37,55 @@ use MasterClub;
         nome varchar(50), 
         descricao varchar(255)
     );
-    create table responsavel_modalidade(
-        id int primary key,
-        modalidade int,
-        foreign key (modalidade) references modalidade(codigo),
-        foreign key (funcionario) references funcionario(id),
-        coordenador varchar(30)
-    );
-    create table clube(
-    id int primary key auto_increment,
-    cnpj char(18),
-    nome varchar(30),
-    quantidade_atletas int,
-    titulo int,
-    foreign key (titulo) references titulo(codigo)
-    );
-    create table participa(
-		clube int primary key auto_increment,
-        modalidade int primary key auto_increment,
-        foreign key (clube) references clube(cnpj),
-        foreign key (modalidade) references modalidade(codigo)
-    );
-	create table titulos(
+    create table titulos(
 		codigo int primary key auto_increment,
         data_titulo date
     );
     create table chefe(
 		id int primary key auto_increment,
         administracao varchar(30)
+    );
+    create table dependencia(
+		id int primary key auto_increment,
+        nome varchar(50),
+        capacidade int
+    );
+         create table clube(
+		id int primary key auto_increment,
+		cnpj char(18),
+		nome varchar(30),
+		quantidade_atletas int,
+		titulo int,
+		foreign key (titulo) references titulos(codigo)
+    );
+	create table atleta(
+		id int primary key auto_increment,
+		posicao varchar(10),
+        lado varchar(10),
+        categoria varchar(10),
+        naturalidade varchar(50),
+        idade int,
+        altura float,
+        peso float,
+        cpf char(11),
+        rg char(10),
+        clube int,
+        foreign key (clube) references clube(id)
+    );
+    create table responsavel_modalidade(
+		id int primary key,
+        funcionario int,
+        modalidade int,
+        foreign key (modalidade) references modalidade(codigo),
+        foreign key (funcionario) references funcionario(id),
+        coordenador varchar(30)
+    );
+    create table participa(
+		id int primary key auto_increment,
+		clube int,
+        modalidade int,
+        foreign key (clube) references clube(id),
+        foreign key (modalidade) references modalidade(codigo)
     );
     create table financas(
 		codigo int primary key auto_increment,
@@ -93,22 +100,19 @@ use MasterClub;
         nome varchar(50)
     );
 	create table patrocinar(
-		patrocinio int primary key auto_increment,
-        clube int primary key auto_increment,
+		id int primary key auto_increment,
+		patrocinio int,
+        clube int,
         valorpatrocinio float,
-        foreign key (clube) references clube(cnpj),
+        foreign key (clube) references clube(id),
         foreign key (patrocinio) references patrocinio(codigo)
     );
-    create table dependencia(
-		id int primary key auto_increment,
-        nome varchar(50),
-        capacidade int
-    );
     create table aluguel(
-		clube int primary key auto_increment ,
-        dependencia int primary key auto_increment,
-        foreign key (clube) references clube(cnpj),
-        foreign key (dependencia) references dependencia(codigo),
+		id int primary key auto_increment,
+		clube int,
+        dependencia int,
+        foreign key (clube) references clube(id),
+        foreign key (dependencia) references dependencia(id),
         dataaluguel date,
         valorTotalAluguel float
     );
